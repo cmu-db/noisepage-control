19f5b963242fba7a82d48e2259fb21312c07e575
Tim Lee <heweil@andrew.cmu.edu>
2022-09-18 20:53:53 -0400

    add CORS rules, vscode debug config, resource directory

--- control_plane/control_plane/settings.py
+++ control_plane/control_plane/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 INSTALLED_APPS += [
@@ -53,12 +54,15 @@ INSTALLED_APPS += [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # SECURITY WARNING: Remove this in production
 
 ROOT_URLCONF = 'control_plane.urls'
 
