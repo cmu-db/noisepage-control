6c8806c4fc282105136aeec0b3b6543120d96b2a
Kushagra Singh <singh.kushagra.1996@gmail.com>
2022-09-27 13:41:21 -0400

    updated urls

--- control_plane/database_manager/services/workload_manager/urls.py
+++ control_plane/database_manager/services/workload_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("collect_workload_callback/", views.collect_workload_callback, name="collect_workload_callback"),
-    path("<str:database_id>", views.workloads) # TODO: Refactor to /databases (adhere to HTTP resources)
-    path("download/<str:workload_id>", views.download_workload)
+    path("<str:database_id>", views.workloads), # TODO: Refactor to /databases (adhere to HTTP resources)
+    path("download/<str:workload_id>", views.download_workload),
 ]
