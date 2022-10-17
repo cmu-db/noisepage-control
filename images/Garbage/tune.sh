03bcb02b8cb0b7c0e6ce804b0ca04466d63eb4c2
Kushagra Singh <singh.kushagra.1996@gmail.com>
2022-10-16 20:02:34 -0400

    added garbage image

---
+++ images/Garbage/tune.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Start postgres
+service postgresql start
+
+# Create user
+echo "CREATE USER ${PG_USERNAME} WITH SUPERUSER;" | psql
+
+# Restore schema; /schema.sql should be mounted while execing 
+psql postgres -U ${PG_USERNAME} < /data/schema.sql 
+
+# Move to directory
+cd /noisepage-pilot/action/generation
+
+# Run tuning engine
+python3 engine.py -o searchspace.json -c /data/garbage_config.yaml
+
+# Move generated file to mounted directory
+mv searchspace.json /data/
