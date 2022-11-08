cb48766edff8ffd92c41dba9fce27b2c4284834b
Tim Lee <heweil@andrew.cmu.edu>
2022-11-08 14:54:00 -0500

    Removes table schema in dump_ddl.sql

---
+++ images/Garbage/remove_table_schema.py
@@ -0,0 +1,43 @@
+import sys
+import re
+
+"""
+Example input:
+CREATE TABLE public.item (
+    i_id integer NOT NULL,
+    title character varying(128) NOT NULL,
+    description character varying(512) DEFAULT NULL::character varying,
+    creation_date timestamp without time zone
+);
+ALTER TABLE ONLY public.review
+    ADD CONSTRAINT review_i_id_fkey FOREIGN KEY (i_id) REFERENCES public.item(i_id) ON DELETE CASCADE;
+....
+
+Example output:
+CREATE TABLE item (
+    i_id integer NOT NULL,
+    title character varying(128) NOT NULL,
+    description character varying(512) DEFAULT NULL::character varying,
+    creation_date timestamp without time zone
+);
+ALTER TABLE ONLY review
+    ADD CONSTRAINT review_i_id_fkey FOREIGN KEY (i_id) REFERENCES public.item(i_id) ON DELETE CASCADE;
+"""
+def remove_table_schema(ddl):
+    # traverse line by line
+    lines = ddl.splitlines()
+    output = ""
+    for line in lines:
+        if line.startswith("CREATE TABLE") or line.startswith("ALTER TABLE"):
+            # remove schema name
+            line = re.sub('[^ ]+\.', '', line)
+        output += f"{line}\n"
+    return output
+
+if __name__ == "__main__":
+    ddl_filename = sys.argv[1]
+    with open(ddl_filename, "r") as fp:
+        ddl = fp.read()
+        ddl = remove_table_schema(ddl)
+    with open(ddl_filename, "w") as fp:
+        fp.write(ddl)
