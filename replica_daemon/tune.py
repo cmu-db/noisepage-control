db4f18a063c16a5775895b9cd52a60de478e8de2
Kushagra Singh <singh.kushagra.1996@gmail.com>
2022-11-14 15:54:38 -0500

    integrated garbage e2e

--- replica_daemon/tune.py
+++ replica_daemon/tune.py
@@ -4,11 +4,11 @@ import os
 from pathlib import Path
 import docker
 import json
-
+import glob
 import requests
 
 # IMAGE_NAME = "kushagr2/garbage:v2" # Only garbage
-IMAGE_NAME = "kushagr2/garbage:v3" # Garbage + Index
+IMAGE_NAME = "kushagr2/garbage:v4" # Garbage + Index
 
 def tune_database(tuning_instance_id, db_name, callback_url):
 
@@ -62,27 +62,32 @@ def tune_database(tuning_instance_id, db_name, callback_url):
             }, 
         detach = False)
 
+    # Parse generated file for actions
+    results_dir_path = parent_dir_path / "data" /  "benchmark_results/"
+
+    actions = []
+    for file in os.listdir(results_dir_path):
+        if not file.endswith(".sql"): # Ignore non SQL files
+            continue
+
+        with open(results_dir_path / file, "r") as fp:
+            for action in fp.readlines():
+                actions.append({
+                    "command": action.strip("\n"),
+                    "benefit": 100.0,
+                    "reboot_required": False if action.startswith("create index") else True,
+                })
+
+
     # Clean up
     os.remove("workload.tar.gz")
     shutil.rmtree(workload_dir_name)
     os.remove("state.tar.gz")
     shutil.rmtree(state_dir_name)
+    shutil.rmtree("data")
 
-
-
-    # Dummy data; TODO: Get from container out
     data = {
-        "actions": [
-            {
-                "command": "CREATE INDEX ppp on resource_manager_resource (available);",
-                "benefit": 300.0,
-                "reboot_required": False
-            }, {
-                "command": "ALTER SYSTEM SET max_connections TO '20';",
-                "benefit": 200.0,
-                "reboot_required": True
-            }
-        ],
+        "actions": actions,
         "tuning_instance_id": tuning_instance_id,
         "exec_logs": exec_logs
     }
