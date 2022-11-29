f9c288185f02ed7c178258ad6cc2a97d4a31b089
Tim Lee <heweil@andrew.cmu.edu>
2022-11-29 10:25:07 -0500

    Put PG image in database list

--- control_plane/client-app/src/pages/MyDatabases/DatabaseInfo.js
+++ control_plane/client-app/src/pages/MyDatabases/DatabaseInfo.js
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
-
 import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import DatabaseRegisterState from '../../util/databaseRegisterState';
 import parseDateTime from '../../util/parseDateTime';
+import postgresLogo from './postgres.png';
 
 export default function DatabaseInfo(props) {
   const { databaseInfo: info } = props;
@@ -22,22 +22,27 @@ export default function DatabaseInfo(props) {
   }
 
   return (
-    <Card sx={{ minWidth: 275, mb: 4 }}>
+    <Card sx={{ minWidth: 275, mb: 4, p: 1, display: 'flex', alignItems: 'center' }}>
+      <img
+        src={postgresLogo}
+        alt="pg-logo"
+        style={{ width: '100px', height: '100px', paddingLeft: '24px', objectFit: 'contain' }}
+      />
       <Grid container spacing={2} alignItems="center" sx={{ p: 2, px: 3 }} >
-        <Grid item xs={12} md={10}>
-          <Typography variant="h4">Database ID: {info.database_id}</Typography>
+        <Grid item xs={12} md={9}>
+          <Typography variant="h4" pt={1}>Database ID: {info.database_id}</Typography>
         </Grid>
-        <Grid item xs={12} md={2}>
+        <Grid item xs={12} md={3}>
           <Typography variant="h6" sx={{ display: 'inline-block' }}>State: &nbsp;</Typography>
           <Typography variant="h6" sx={{ display: 'inline-block' }} color={getStateColor(info.state)}>{DatabaseRegisterState[info.state]}</Typography>
         </Grid>
-        <Grid item xs={12} md={10}>
+        <Grid item xs={12} md={9}>
           <Link to={`/databases/${info.database_id}`}>
-            <Button>Manage</Button>
+            <Button sx={{ pt: 0 }}>Manage</Button>
           </Link>
         </Grid>
-        <Grid item xs={12} md={2}>
-          <Typography>Created At: {parseDateTime(info.created)}</Typography>
+        <Grid item xs={12} md={3}>
+          <Typography sx={{ fontSize: '0.9rem', pt: 0 }}>Created At: {parseDateTime(info.created)}</Typography>
         </Grid>
       </Grid>
     </Card>
